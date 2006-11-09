9c5e95fdf54ec16ebfd32ffee9e805f196144562
leeten <leeten@abdb6057-6f73-fb73-b203-3162f873fe11>
2006-11-09 19:22:31 +0000

    [11/09/2006] 1. First time checkin.

---
+++ libfbo/fbo.h
@@ -0,0 +1,17 @@
+#ifndef __FBO__H__
+#define __FBO__H__
+
+GLenum EGetFboStatus(bool bPrint=false);
+
+#endif // __FBO__H__
+
+/*
+
+  $Log: not supported by cvs2svn $
+  Revision 1.1.1.1  2006/09/13 19:49:26  leeten
+
+  [09/13/2006]
+  1. First time checkin. It is a new version renderere which simulate 4 slices at the same time.
+
+
+*/
