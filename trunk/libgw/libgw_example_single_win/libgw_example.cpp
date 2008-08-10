b9eec014cc6368b93884caa7b6cacda2511f90fa
leeten <leeten@abdb6057-6f73-fb73-b203-3162f873fe11>
2008-08-10 05:03:56 +0000

    [2008/08/10] 1. Include GlutWin.h to replace gw.h 2. Create three windows. 3. Use the static member CGlutWin::_AddWin to create windows.

--- trunk/libgw/libgw_example_single_win/libgw_example.cpp
+++ trunk/libgw/libgw_example_single_win/libgw_example.cpp
@@ -1,6 +1,11 @@
-#include "gw.h"
+// MOD-BY-LEETEN 08/09/2008-FROM:
+	// #include "gw.h"
+// TO:
+#include "GlutWin.h"
+// MOD-BY-LEETEN 08/09/2008-END
 
 class CTestWin:public CGlutWin {
+	// the colors
 	float r, g, b, a;
 
 	void _DisplayFunc()
@@ -24,7 +29,10 @@ public:
 	}
 };
 
-CTestWin cGreenWin(0.0, 1.0, 0.0, 1.0), cRedWin(1.0, 0.0, 0.0, 1.0);
+CTestWin 
+	cGreenWin(0.0, 1.0, 0.0, 1.0), 
+	cRedWin(1.0, 0.0, 0.0, 1.0),
+	cBlueWin(0.0, 0.0, 1.0, 1.0);
 
 int
 main(int argn, char *argv[])
@@ -32,8 +40,15 @@ main(int argn, char *argv[])
 	glutInit(&argn, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA | GLUT_ALPHA | GLUT_STENCIL );
 
-	_GWAddWin(&cGreenWin,	"GREEN");
-	_GWAddWin(&cRedWin,		"RED");
+	#if 0	// MOD-BY-LEETEN 08/09/2008-FROM:
+		_GWAddWin(&cRedWin,		"RED");
+		_GWAddWin(&cGreenWin,	"GREEN");
+		_GWAddWin(&cBlueWin,	"BLUE");
+	#else	// MOD-BY-LEETEN 08/09/2008-TO:
+	CGlutWin::_AddWin(&cRedWin,		"RED");
+	CGlutWin::_AddWin(&cGreenWin,	"GREEN");
+	CGlutWin::_AddWin(&cBlueWin,	"BLUE");
+	#endif	// MOD-BY-LEETEN 08/09/2008-END
 
 	glutMainLoop();
 	return 0;
@@ -42,5 +57,10 @@ main(int argn, char *argv[])
 /*
 
 $Log: not supported by cvs2svn $
+Revision 1.1.1.1  2008/08/09 13:50:10  leeten
+
+[2008/08/09]
+1. First time checkin. LIBGW is a OO-based library to create GLUT window. In each window, there is a 3D coordinate for the object and mouse countrol to manipulate the coordinate and viewpoint.
+
 
 */
