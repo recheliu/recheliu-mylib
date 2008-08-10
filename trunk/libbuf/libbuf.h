a52f0b4a061c77f4fb77edb6f041f5209fcaf1ef
leeten <leeten@abdb6057-6f73-fb73-b203-3162f873fe11>
2008-08-10 18:54:05 +0000

    [2008/08/10] 1. First time checkin. This lib. is used to wrap a buffer in order to avoid the routine to check and free memory.

---
+++ trunk/libbuf/libbuf.h
@@ -0,0 +1,67 @@
+/*
+
+This file defines several important macros for CUDA.
+
+*/
+
+#ifndef __LIB_BUFFER_H__
+#define __LIB_BUFFER_H__
+
+#include <memory.h>
+#include <assert.h>
+
+// a template for buffer
+template <typename t> struct TBuffer 
+{
+	int num;
+	t* data;
+
+	TBuffer()	
+	{	
+		num = 0;
+		data = NULL;	
+	}
+
+	~TBuffer()	
+	{	
+		if( data ) 
+			// free(data);	
+			delete [] data;
+		data = NULL;
+	}
+
+	t* alloc(int n)
+	{
+		assert(n > 0);
+		num = n;
+
+		// data = (t*)calloc(num, sizeof(t));
+		data = new t[num];
+		assert(data);
+		memset(data, 0, sizeof(t) * num);
+		return data;
+	}
+
+	t& operator[](int index)
+	{
+		assert( index < num );
+		return data[index];
+	}
+
+	const t& operator[](int index) const
+	{
+		assert( index < num );
+		return data[index];
+	}
+};
+
+#endif	// __LIB_BUFFER_H__
+
+/*
+
+$Log: not supported by cvs2svn $
+
+*/
+
+
+
