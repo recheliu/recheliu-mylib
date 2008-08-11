b3056b0055af14d940981a1c917273048804376b
leeten <leeten@abdb6057-6f73-fb73-b203-3162f873fe11>
2008-08-11 04:35:06 +0000

    [2008/08/11] 1. [CHANGE] Declare the color variables as public. 2. [ADD] Add a new window to draw a 3D white box. This window is associated w/ a GLUI win and a GLUI subwin to control the RGB channels. 3. [ADD] Add a GLUI window to the GREEN window to change the green channel. 4. [ADD] Add a GLUI sub-window to the BLUE window to change the blue channel.

--- trunk/libgw/libgw_example_single_win/libgw_example.cpp
+++ trunk/libgw/libgw_example_single_win/libgw_example.cpp
@@ -6,6 +6,12 @@
 
 class CTestWin:public CGlutWin {
 	// the colors
+
+// ADD-BY-LEETEN 08/11/2008-BEGIN
+// declare them as public methods and variables
+public:
+// ADD-BY-LEETEN 08/11/2008-END
+
 	float r, g, b, a;
 
 	void _DisplayFunc()
@@ -34,6 +40,10 @@ CTestWin
 	cRedWin(1.0, 0.0, 0.0, 1.0),
 	cBlueWin(0.0, 0.0, 1.0, 1.0);
 
+// ADD-BY-LEETEN 08/11/2008-BEGIN
+CTestWin cWhiteWin(1.0, 1.0, 1.0, 1.0);
+// ADD-BY-LEETEN 08/11/2008-END
+
 int
 main(int argn, char *argv[])
 {
@@ -45,9 +55,31 @@ main(int argn, char *argv[])
 		_GWAddWin(&cGreenWin,	"GREEN");
 		_GWAddWin(&cBlueWin,	"BLUE");
 	#else	// MOD-BY-LEETEN 08/09/2008-TO:
-	CGlutWin::_AddWin(&cRedWin,		"RED");
-	CGlutWin::_AddWin(&cGreenWin,	"GREEN");
-	CGlutWin::_AddWin(&cBlueWin,	"BLUE");
+
+	#if	0	// MOD-BY-LEETEN 08/11/2008-FROM:
+		CGlutWin::_AddWin(&cRedWin,		"RED");
+		CGlutWin::_AddWin(&cGreenWin,	"GREEN");
+		CGlutWin::_AddWin(&cBlueWin,	"BLUE");
+	#else	// MOD-BY-LEETEN 08/11/2008-TO:
+
+	CGlutWin::_AddWin(&cRedWin,		"RED", false, 100, 100, 256, 256);
+
+	cGreenWin._AddGluiWin();
+	CGlutWin::_AddWin(&cGreenWin,	"GREEN", false, 200, 100, 256, 256);
+	cGreenWin.PCGetGluiWin()->add_spinner("G:", GLUI_SPINNER_FLOAT, &cGreenWin.g);
+
+	cBlueWin._AddGluiSubwin(GLUI_SUBWINDOW_RIGHT);
+	CGlutWin::_AddWin(&cBlueWin,	"BLUE", false, 300, 100, 256, 256);
+		cBlueWin.PCGetGluiSubwin()->add_spinner("B:", GLUI_SPINNER_FLOAT, &cBlueWin.b);
+
+	cWhiteWin._AddGluiWin();
+	cWhiteWin._AddGluiSubwin(GLUI_SUBWINDOW_LEFT);
+	CGlutWin::_AddWin(&cWhiteWin,	"WHITE", false, 400, 100, 256, 256);
+		cWhiteWin.PCGetGluiSubwin()->add_spinner("R:", GLUI_SPINNER_FLOAT, &cWhiteWin.r);
+		cWhiteWin.PCGetGluiSubwin()->add_spinner("G:", GLUI_SPINNER_FLOAT, &cWhiteWin.g);
+		cWhiteWin.PCGetGluiWin()->add_spinner("B:", GLUI_SPINNER_FLOAT, &cWhiteWin.b);
+
+	#endif	// MOD-BY-LEETEN 08/11/2008-END
 	#endif	// MOD-BY-LEETEN 08/09/2008-END
 
 	glutMainLoop();
@@ -57,6 +89,13 @@ main(int argn, char *argv[])
 /*
 
 $Log: not supported by cvs2svn $
+Revision 1.2  2008/08/10 05:03:56  leeten
+
+[2008/08/10]
+1. Include GlutWin.h to replace gw.h
+2. Create three windows.
+3. Use the static member CGlutWin::_AddWin to create windows.
+
 Revision 1.1.1.1  2008/08/09 13:50:10  leeten
 
 [2008/08/09]
