5d2e4cfe3b3627c5820903a8510aeda615530edd
leeten <leeten@abdb6057-6f73-fb73-b203-3162f873fe11>
2008-08-14 22:54:48 +0000

    [2008/08/14] 1. [FIRST TIME CHECKIN] This is a library to create a GLUTWIN window for direct volume rendering (DVR).

---
+++ trunk/libdvr/DvrWin.cpp
@@ -0,0 +1,506 @@
+#include <GL/glew.h>
+#include "shader.h"
+#include "libtf2.h"
+
+#include "DvrWin.h"
+
+CDvrWin::CDvrWin(void)
+{
+	t3dVol = 0;
+	t1dTf = 0;
+	t2dScreenColor = 0;
+	t2dScreenDepth = 0;
+
+						// shader object handle
+	uPidTf1d = 0;		// handle to the shader that executes classification via 1D transfer function
+
+						// FBO handle
+	uFidScreen = 0;
+
+	iMaxNrOfSlices = 4096;
+	iNrOfSlices = 128;
+
+						// dim. of the volume
+	iXDim = 128;
+	iYDim = 128;
+	iZDim = 128;
+
+						// the gain of the thickness
+	fThicknessRatio = 1.0f;
+
+						// not used FBO by default
+	ibRenderToFbo = false;
+
+	// ADD-BY-TLEE 2008/08/14-BEGIN
+	_AddGluiWin();				// there is a sub GLUI window
+	// ADD-BY-TLEE 2008/08/14-END
+}
+
+CDvrWin::~CDvrWin(void)
+{
+}
+
+void
+CDvrWin::_DisplayVolume()
+{
+	assert(t3dVol);
+	assert(t1dTf);
+
+	glPushAttrib(GL_ENABLE_BIT);
+	glEnable( GL_DEPTH_TEST);
+	glEnable( GL_BLEND);
+	glEnable( GL_TEXTURE_GEN_S );
+	glEnable( GL_TEXTURE_GEN_T );
+	glEnable( GL_TEXTURE_GEN_R );
+	glEnable( GL_TEXTURE_GEN_Q );
+
+								// step-1:
+								// setup clipping planes
+	static GLdouble pdClipPlanes[][4] = {
+		{-1.0, 0.0, 0.0, 1.0},
+		{ 1.0, 0.0, 0.0, 1.0},
+		{ 0.0,-1.0, 0.0, 1.0},
+		{ 0.0, 1.0, 0.0, 1.0},
+		{ 0.0, 0.0,-1.0, 1.0},
+		{ 0.0, 0.0, 1.0, 1.0},
+	};
+
+	for(int i=0; i<sizeof(pdClipPlanes)/sizeof(pdClipPlanes[0]); i++) 
+	{
+		glEnable(GL_CLIP_PLANE0+i);
+		glClipPlane(GL_CLIP_PLANE0+i, pdClipPlanes[i]);
+	}
+
+								// step-2:
+								// setup texture generation 
+	glActiveTexture(GL_TEXTURE0);
+	glTexGeni( GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR );
+	glTexGeni( GL_T, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR );
+	glTexGeni( GL_R, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR );
+	glTexGeni( GL_Q, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR );
+
+	static TMatrix tTextureMatrix = { // this matrix is used to convert the coordinate from [-1, +1] to [0.0, 1.0].
+		0.5f, 0.0f, 0.0f, 0.5f,
+		0.0f, 0.5f, 0.0f, 0.5f,
+		0.0f, 0.0f, 0.5f, 0.5f,
+		0.0f, 0.0f, 0.0f, 1.0f,
+	};
+	glTexGendv( GL_S, GL_OBJECT_PLANE, &tTextureMatrix[0] );
+	glTexGendv( GL_T, GL_OBJECT_PLANE, &tTextureMatrix[4] );
+	glTexGendv( GL_R, GL_OBJECT_PLANE, &tTextureMatrix[8] );
+	glTexGendv( GL_Q, GL_OBJECT_PLANE, &tTextureMatrix[12] );
+
+								// step-3:
+								// find the bounding box in eye space
+	static GLdouble pdCornerCoord[][3] = {
+		{-1.0, -1.0, -1.0},
+		{-1.0, -1.0,  1.0},
+		{-1.0,  1.0, -1.0},
+		{-1.0,  1.0,  1.0},
+		{ 1.0, -1.0, -1.0},
+		{ 1.0, -1.0,  1.0},
+		{ 1.0,  1.0, -1.0},
+		{ 1.0,  1.0,  1.0},
+	};
+
+	double dMinX, dMinY, dMinZ;
+	double dMaxX, dMaxY, dMaxZ;
+	TMatrix tModelviewMatrix;
+
+	dMinX = dMinY = dMinZ = HUGE_VAL;
+	dMaxX = dMaxY = dMaxZ = -HUGE_VAL;
+	glGetDoublev(GL_MODELVIEW_MATRIX, tModelviewMatrix);
+	for(int i=0; i<sizeof(pdCornerCoord)/sizeof(pdCornerCoord[0]); i++) 
+	{
+		double dX, dY, dZ;
+		gluProject(
+			pdCornerCoord[i][0], pdCornerCoord[i][1], pdCornerCoord[i][2],
+			tModelviewMatrix, tProjectionMatrix, piViewport,
+			&dX, &dY, &dZ);
+
+		if( dX < dMinX )	dMinX = dX;
+		if( dX > dMaxX )	dMaxX = dX;
+
+		if( dY < dMinY )	dMinY = dY;
+		if( dY > dMaxY )	dMaxY = dY;
+
+		if( dZ < dMinZ )	dMinZ = dZ;
+		if( dZ > dMaxZ )	dMaxZ = dZ;
+	}
+								// bind the 3D texture of the volume
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_3D, t3dVol);	
+
+								// bind the 1D texture of the transfer func.
+	glActiveTexture(GL_TEXTURE2);
+	glBindTexture(GL_TEXTURE_1D, t1dTf);	
+
+								// launch the shader object
+	glUseProgramObjectARB(uPidTf1d);
+
+	float fThickness;
+	fThickness = fThicknessRatio * (float)(dMaxZ - dMinZ) / (float)iNrOfSlices;
+	SET_1F_VALUE_BY_NAME(uPidTf1d, "fThickness", fThickness);
+	SET_1F_VALUE_BY_NAME(uPidTf1d, "fWindowWidth", (float)piViewport[2]);
+	SET_1F_VALUE_BY_NAME(uPidTf1d, "fWindowHeight", (float)piViewport[3]);
+
+
+	glColor4d(1.0, 1.0, 1.0, 1.0 / (double)iNrOfSlices);
+	for(int z=0; z<=iNrOfSlices; z++)
+	{
+		double dX, dY, dZ;
+
+		double dColor = (double)z / (double)iNrOfSlices;
+		double dDepth = (1.0 - (double)z / (double)iNrOfSlices) * (dMaxZ - dMinZ) + dMinZ; 
+
+		glBegin(GL_QUADS);
+			gluUnProject(dMinX, dMinY, dDepth,
+				tModelviewMatrix, tProjectionMatrix, piViewport,
+				&dX, &dY, &dZ);
+			glVertex3d(dX, dY, dZ);
+
+			gluUnProject(dMaxX, dMinY, dDepth,
+				tModelviewMatrix, tProjectionMatrix, piViewport,
+				&dX, &dY, &dZ);
+			glVertex3d(dX, dY, dZ);
+
+			gluUnProject(dMaxX, dMaxY, dDepth,
+				tModelviewMatrix, tProjectionMatrix, piViewport,
+				&dX, &dY, &dZ);
+			glVertex3d(dX, dY, dZ);
+
+			gluUnProject(dMinX, dMaxY, dDepth,
+				tModelviewMatrix, tProjectionMatrix, piViewport,
+				&dX, &dY, &dZ);
+			glVertex3d(dX, dY, dZ);
+		glEnd();
+	}
+
+	glPopAttrib();	// glPushAttrib(GL_ENABLE_BIT);	
+
+	// disable the fragment shader
+	glUseProgramObjectARB(0);
+
+	// reset the active texture to texture 0
+	glActiveTexture(GL_TEXTURE0);
+}
+
+///////////////////////////////////////////////////////////////
+// GLUT Callbacks 
+void
+CDvrWin::_DisplayFunc()
+{
+	if( ibRenderToFbo )
+		// bind the FBO
+		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, uFidScreen);
+
+	// clear the depth/color buffer
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// to maintain the aspect ratio, the coordinate is normalized 
+	glScaled(1.0, (double)iYDim/(double)iXDim, (double)iZDim/(double)iXDim);
+
+	// draw the bounding box
+	glPushAttrib(GL_LINE_BIT);
+	glLineWidth(2.0f);
+
+		glColor4f(0.6f, 0.6f, 0.2f, 1.0f);
+		glutWireCube(2.0);
+
+	glPopAttrib();	// glPushAttrib(GL_LINE_BIT);
+
+	// apply direct volume rendering 
+	_DisplayVolume();
+
+	// bind the buffer back and render the FBO as texture on current screen
+	if( ibRenderToFbo )
+	{
+		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, t2dScreenColor);
+		glEnable(GL_TEXTURE_2D);
+
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+
+		glBegin(GL_QUADS);
+			glTexCoord2f(0.0f, 0.0f);	glVertex2f(-1.0f, -1.0f);
+			glTexCoord2f(1.0f, 0.0f);	glVertex2f( 1.0f, -1.0f);
+			glTexCoord2f(1.0f, 1.0f);	glVertex2f( 1.0f,  1.0f);
+			glTexCoord2f(0.0f, 1.0f);	glVertex2f(-1.0f,  1.0f);
+		glEnd();
+
+		glDisable(GL_TEXTURE_2D);
+	}
+
+	// DEL-BY-LEETEN 08/12/2008-BEGIN
+		// glutSwapBuffers();
+	// DEL-BY-LEETEN 08/12/2008-END
+}
+
+#if	0	// DEL-BY-LEETEN 08/14/2008-BEGIN
+	void
+	CDvrWin::_IdleFunc()
+	{
+	}
+#endif	// DEL-BY-LEETEN 08/12/2008-END
+
+// ADD-BY-LEETEN 02/25/2008-BEGIN
+void
+CDvrWin::_ReshapeFunc(int w, int h)
+{
+	glViewport(0, 0, w, h);
+
+	// allocate new FBO
+	if( w && h )
+	{
+		if( ibRenderToFbo )
+		{
+			glBindTexture(GL_TEXTURE_2D, t2dScreenColor);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F_ARB,
+				w, h, 0, GL_RGBA, GL_FLOAT, NULL);	
+
+			glBindTexture(GL_TEXTURE_2D, t2dScreenDepth);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT24,
+				w, h, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);	
+
+			glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, uFidScreen);
+			glFramebufferTexture2DEXT(
+				GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT,
+				GL_TEXTURE_2D, t2dScreenColor, 0);
+
+			glFramebufferTexture2DEXT(
+				GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT,
+				GL_TEXTURE_2D, t2dScreenDepth, 0);
+
+			assert( GL_FRAMEBUFFER_COMPLETE_EXT == EGetFboStatus(true) );
+
+			glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+		}
+	}
+}
+
+void 
+CDvrWin::_SpecialFunc(int skey, int x, int y)
+{
+}
+
+void
+CDvrWin::_KeyboardFunc(unsigned char key, int x, int y)
+{
+	switch(key) 
+	{
+	case 'F': case 'f':	// F fior Fullscreen
+		static bool bFullScreen;
+		static int iWidth, iHeight;
+		static int iWinPosX, iWinPosY;
+
+		bFullScreen = !bFullScreen;
+		if ( bFullScreen ) 
+		{
+			iWidth = glutGet( GLUT_WINDOW_WIDTH );
+			iHeight = glutGet( GLUT_WINDOW_HEIGHT );
+			iWinPosX = glutGet( GLUT_WINDOW_X );
+			iWinPosY = glutGet( GLUT_WINDOW_Y );
+			glutFullScreen();
+		} 
+		else
+		{
+			glutReshapeWindow(iWidth, iHeight);
+			glutPositionWindow(iWinPosX, iWinPosY);
+		}
+
+		break;
+
+	/*
+	case 'M': case 'm':	// M for matrix
+		fprintf(stderr, "Outputing modelview matrix to file %s...", szMatrixFilename);
+
+		FILE *fpMatrix;
+		fopen_s(&fpMatrix , szMatrixFilename, "wt");
+		assert( fpMatrix );
+		for(int i = 0, r = 0; r < 4; r++)
+		{
+			for(int c = 0; c < 4; c++, i++)
+				fprintf(fpMatrix, "%f ", tViewMatrix[i]);
+			fprintf(fpMatrix, "\n");
+		}
+
+		for(int i = 0, r = 0; r < 4; r++)
+		{
+			for(int c = 0; c < 4; c++, i++)
+				fprintf(fpMatrix, "%f ", tModelMatrix[i]);
+			fprintf(fpMatrix, "\n");
+		}
+		fclose( fpMatrix );
+		fprintf(stderr, "done\n");
+		break;
+	*/
+
+	// double #slices
+	case '*':
+		if( iNrOfSlices * 2 <= iMaxNrOfSlices )
+			iNrOfSlices *= 2;
+		fprintf(stderr, "#Slices = %d\n", iNrOfSlices);
+
+		glutPostRedisplay();
+		break;
+
+	// halve #slices
+	case '/':
+		if( iNrOfSlices / 2 > 0 )
+			iNrOfSlices /= 2;
+		fprintf(stderr, "#Slices = %d\n", iNrOfSlices);
+		glutPostRedisplay();
+		break;
+
+	// change the thickness
+	case '+':
+		fThicknessRatio *= 2.0f;
+		fprintf(stderr, "Thickness Ratio = %f\n", fThicknessRatio);
+		glutPostRedisplay();
+		break;
+
+	// change the thickness
+	case '-':
+		if(	fThicknessRatio > 0.0f )
+			fThicknessRatio /= 2.0f;
+		fprintf(stderr, "Thickness Ratio = %f\n", fThicknessRatio);
+		glutPostRedisplay();
+		break;
+	}
+}
+
+///////////////////////////////////////////////////////////////
+// basic subroutines
+
+void
+CDvrWin::_InitFunc()
+{
+	// initialize GLEW and check required OpenGL extension
+	assert( GLEW_OK == glewInit() );
+	assert( GLEW_ARB_vertex_shader && GLEW_ARB_fragment_shader );
+	assert( GLEW_ARB_texture_non_power_of_two );
+	assert( GL_EXT_texture3D );
+	assert( GL_EXT_framebuffer_object );
+
+
+	// load the shader
+	uPidTf1d = CSetShaders(NULL, "tf_1d.frag");
+	assert( uPidTf1d );
+
+	// setup value for variable inside the shader
+	glUseProgramObjectARB(uPidTf1d);
+	SET_1I_VALUE_BY_NAME(uPidTf1d, "t3dVol",	0);
+	SET_1I_VALUE_BY_NAME(uPidTf1d, "t1dTf",	2);
+	glUseProgramObjectARB(0);
+
+	// setup the blending equation for back-to-front composition
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	// allocate the textures for FBOs
+	if( ibRenderToFbo )
+	{
+		CREATE_2D_TEXTURE(
+			GL_TEXTURE_2D, t2dScreenColor, 
+			GL_NEAREST, GL_RGBA16F_ARB, 
+			128, 128, // the real resolution will be determined later
+			GL_RGBA, GL_FLOAT, NULL);
+
+		CREATE_2D_TEXTURE(
+			GL_TEXTURE_2D, t2dScreenDepth, 
+			GL_NEAREST, GL_DEPTH_COMPONENT24,
+			128, 128, // the real resolution will be determined later
+			GL_DEPTH_COMPONENT, GL_FLOAT, NULL); 
+
+		glGenFramebuffersEXT(1, &uFidScreen);	// allocate framebuffer object
+	}
+	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+
+	// ADD-BY-TLEE 2008/08/14-BEGIN
+
+	_DisplayFpsOn();		// the FPS will be displayed 
+	_KeepUpdateOn();		// the frame will be keep updating
+
+							// a spinner to control the #slices
+	PCGetGluiWin()->add_spinner("#Slices = ", GLUI_SPINNER_INT, &iNrOfSlices);	
+
+	// ADD-BY-TLEE 2008/08/14-END
+}
+
+// load the volume and upload it as a 3D texture 
+void 
+CDvrWin::_SetVolume(
+	const void *pVol, 
+	GLenum eType, 
+	GLenum eFormat, 
+	int iXDim, 
+	int iYDim, 
+	int iZDim)
+{
+_Begin();
+
+	this->iXDim = iXDim;
+	this->iYDim = iYDim;
+	this->iZDim = iZDim;
+
+	glActiveTexture(GL_TEXTURE0);
+
+	if( !t3dVol )
+		glGenTextures(1, &t3dVol);	
+
+	glBindTexture(GL_TEXTURE_3D, t3dVol);	
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);	
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);	
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);	
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);	
+	glTexImage3D(GL_TEXTURE_3D, 0, GL_LUMINANCE16F_ARB,	
+		(GLsizei)iXDim, (GLsizei)iYDim, (GLsizei)iZDim, 0, eType, eFormat, pVol);
+
+_End();
+}
+
+// load the transfer function and upload it as a 1D texture
+void 
+CDvrWin::_SetTransferFunc(
+	const void *pTf, 
+	GLenum eType, 
+	GLenum eFormat, 
+	int iNrOfTfEntries)
+{
+_Begin();
+
+	// upload the transfer func. as a 1D texture
+	glActiveTexture(GL_TEXTURE2);
+	if( !t1dTf )
+		glGenTextures(1, &t1dTf);	
+	glBindTexture(GL_TEXTURE_1D, t1dTf);	
+	glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	
+	glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);	
+	glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);	
+	glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA16F_ARB,	
+		iNrOfTfEntries, 0, eType, eFormat, pTf);
+
+_End();
+}
+
+/*
+
+$Log: not supported by cvs2svn $
+Revision 1.2  2008/08/13 21:16:31  leeten
+
+[2008/08/13]
+1. [DEL] Remove glutSwapBuffers();
+
+Revision 1.1.1.1  2008/08/12 16:58:49  leeten
+
+[2008/08/12]
+1. First time checkin. This is my DVR renderer for 3D regular volume. For testing, the dataset HIPIP is checkin too.
+
+
+*/
+
---
+++ trunk/libdvr/DvrWin.h
@@ -0,0 +1,92 @@
+/**************************************************
+
+CDvrWin:
+
+This file defines a class for apply direct volume rendering (DVR) to a 
+regular 3D volume. 
+
+It is a subclass of CGlutWin, a class for creating GLUT/GLUI-based
+display window. 
+
+The proecduer to use this class is as below:
+
+1. declare a object of class CDvrWin.
+2. Set up a volume and a transfer function before entering the GLUT main loop. 
+(NOTE: both the volume and transfer function can be alternated later)
+
+**************************************************/
+
+#pragma comment (lib, "glew32.lib")      
+#pragma comment (lib, "libfbo.lib")      
+#pragma comment (lib, "libshader.lib")      
+#pragma comment (lib, "libdvr.lib")      
+
+#pragma once
+#include "GlutWin.h"
+
+class CDvrWin :
+	public CGlutWin
+{
+	// inhered interfaces
+	void _DisplayFunc();
+	void _ReshapeFunc(int, int);
+	void _KeyboardFunc(unsigned char, int, int);
+	void _SpecialFunc(int, int, int);
+
+#if	0	// DEL-BY-LEETEN 08/14/2008-BEGIN
+		void _IdleFunc();
+	#endif	// DEL-BY-LEETEN 08/14/2008-END
+
+	void _InitFunc();
+
+	// new methods
+	void _DisplayVolume();
+
+public:
+	// texture handle
+	GLuint t3dVol;
+	GLuint t1dTf;
+	GLuint t2dScreenColor;
+	GLuint t2dScreenDepth;
+
+	// shader object handles
+	GLuint uPidTf1d;	// the handle to the shader that implement 1D transfer function classification
+
+	// FBO handle
+	GLuint uFidScreen;
+
+	// #slices
+	int iNrOfSlices;
+
+	// #max slices
+	int iMaxNrOfSlices;
+
+	// dim. of the volume
+	int iXDim, iYDim, iZDim;
+
+	// the gain of the thickness
+	float fThicknessRatio;
+
+	// a flag to indicate if FBO is used
+	int ibRenderToFbo;
+
+	void _SetVolume(const void *pVol, GLenum eType, GLenum eFormat, int iXDim, int iYDim, int iZDim);
+	void _SetTransferFunc(const void *pTf, GLenum eType, GLenum eFormat, int iNrOfTfEntries);
+
+public:
+	CDvrWin(void);
+public:
+	~CDvrWin(void);
+};
+
+/*
+
+$Log: not supported by cvs2svn $
+Revision 1.1.1.1  2008/08/12 16:58:49  leeten
+
+[2008/08/12]
+1. First time checkin. This is my DVR renderer for 3D regular volume. For testing, the dataset HIPIP is checkin too.
+
+
+*/
+
