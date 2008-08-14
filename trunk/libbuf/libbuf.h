af4128eb708233cc1b8c9ec68b2e54ab6c652607
leeten <leeten@abdb6057-6f73-fb73-b203-3162f873fe11>
2008-08-14 22:52:24 +0000

    [2008/08/14] 1. [ADD] Add a new method free() to explictly free the allocated buffer.

--- trunk/libbuf/libbuf.h
+++ trunk/libbuf/libbuf.h
@@ -22,18 +22,37 @@ template <typename t> struct TBuffer
 		data = NULL;	
 	}
 
-	~TBuffer()	
-	{	
+	// ADD-BY-TLEE 2008/08/14-BEGIN
+	void free()
+	{
 		if( data ) 
-			// free(data);	
 			delete [] data;
 		data = NULL;
+	}
+	// ADD-BY-TLEE 2008/08/14-END
+
+	~TBuffer()	
+	{	
+		#if	0	// MOD-BY-TLEE 2008/08/14-FROM:
+			if( data ) 
+				// free(data);	
+				delete [] data;
+			data = NULL;
+		#else	// MOD-BY-TLEE 2008/08/14-TO:
+
+		free();
+
+		#endif	// MOD-BY-TLEE 2008/08/14-END
 	}
 
 	t* alloc(int n)
 	{
 		assert(n > 0);
 		num = n;
+
+		// ADD-BY-TLEE 2008/08/14-BEGIN
+		free();
+		// ADD-BY-TLEE 2008/08/14-END
 
 		// data = (t*)calloc(num, sizeof(t));
 		data = new t[num];
@@ -60,6 +79,11 @@ template <typename t> struct TBuffer
 /*
 
 $Log: not supported by cvs2svn $
+Revision 1.1.1.1  2008/08/10 18:54:05  leeten
+
+[2008/08/10]
+1. First time checkin. This lib. is used to wrap a buffer in order to avoid the routine to check and free memory.
+
 
 */
 
