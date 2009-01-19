24d785055df9e6327355ef23efcf816cea7fc65c
leeten <leeten@abdb6057-6f73-fb73-b203-3162f873fe11>
2009-01-19 17:04:58 +0000

    [2009/01/19] 1. [ADD] Include the CUDA builtin vectors. 2. [ADD] Define new macros CREATE_1D_TEXTURE and CREATE_3D_TEXTURE. 3. [ADD] Define a new structure CVectorArray.

--- trunk/libopengl/libopengl.h
+++ trunk/libopengl/libopengl.h
@@ -1,7 +1,48 @@
 #ifndef __LIB_OPENGL_LIB_H_
 #define __LIB_OPENGL_LIB_H_
 
+// ADD-BY-LEETEN 2009/01/18-BEGIN
+	#include "libbuf.h"
+
+						// incldue the CUDA's vector types
+	#include <vector_types.h>
+
+	template<typename VT, typename CT, typename TT>
+	typedef struct CVectorArray {
+		TBuffer<VT> pvCoords;
+		TBuffer<CT> pvColors;
+		TBuffer<TT> pvTexCoords;
+	} CVectorArray ;
+
+// ADD-BY-LEETEN 2009/01/18-END
+
 // Definition of macros
+// ADD-BY-LEETEN 2009/01/18-BEGIN
+#define CREATE_3D_TEXTURE(TARGET, TID, FILTER, INT_FORMAT, WIDTH, HEIGHT, DEPTH, FORMAT, TYPE, PTR)	\
+		{\
+		glGenTextures(1, &(TID));	\
+		glBindTexture(TARGET, TID);	\
+		glTexParameteri(TARGET, GL_TEXTURE_WRAP_S, GL_CLAMP);	\
+		glTexParameteri(TARGET, GL_TEXTURE_WRAP_T, GL_CLAMP);	\
+		glTexParameteri(TARGET, GL_TEXTURE_WRAP_R, GL_CLAMP);	\
+		glTexParameteri(TARGET, GL_TEXTURE_MAG_FILTER, FILTER);	\
+		glTexParameteri(TARGET, GL_TEXTURE_MIN_FILTER, FILTER);	\
+		glTexImage3D(TARGET, 0, INT_FORMAT,	\
+			(WIDTH), (HEIGHT), (DEPTH) 0, (FORMAT), (TYPE), (PTR));	\
+		}
+
+#define CREATE_1D_TEXTURE(TARGET, TID, FILTER, INT_FORMAT, WIDTH, FORMAT, TYPE, PTR)	\
+		{\
+		glGenTextures(1, &(TID));	\
+		glBindTexture(TARGET, TID);	\
+		glTexParameteri(TARGET, GL_TEXTURE_WRAP_S, GL_CLAMP);	\
+		glTexParameteri(TARGET, GL_TEXTURE_MAG_FILTER, FILTER);	\
+		glTexParameteri(TARGET, GL_TEXTURE_MIN_FILTER, FILTER);	\
+		glTexImage1D(TARGET, 0, INT_FORMAT,	\
+			(WIDTH), 0, (FORMAT), (TYPE), (PTR));	\
+		}
+// ADD-BY-LEETEN 2009/01/18-END
+
 #define CREATE_2D_TEXTURE(TARGET, TID, FILTER, INT_FORMAT, WIDTH, HEIGHT, FORMAT, TYPE, PTR)	\
 		{\
 		glGenTextures(1, &(TID));	\
@@ -32,6 +73,11 @@ GLenum EGetFboStatus(bool bPrint=false);
 /*
 
 $Log: not supported by cvs2svn $
+Revision 1.3  2008/08/15 15:08:25  leeten
+
+[2008/08/15]
+1. [CHANGE] In CREATE_2D_TEXTURE, use GL_CLAMP instead GL_CLAMP_TO_EDGE since GL_CLAMP_TO_EDGE is only defined in GLEW.
+
 Revision 1.2  2008/02/23 03:02:33  leeten
 
 [02/22/2008]
