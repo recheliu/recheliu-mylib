ccd18e50c10b6d142551b6e22c42cc7fd5691e49
leeten <leeten@abdb6057-6f73-fb73-b203-3162f873fe11>
2009-08-26 16:01:12 +0000

    [2009/08/26] 1. [1ST] First time checkin.

---
+++ trunk/libclip/ClipWin.cpp
@@ -0,0 +1,323 @@
+#include "ClipWin.h"
+
+void 
+CClipWin::_SetBBox(
+	float fX0, float fY0, float fZ0,
+	float fX1, float fY1, float fZ1)
+{
+	cBBox.f3LowerCorner.x = fX0;
+	cBBox.f3LowerCorner.y = fY0;
+	cBBox.f3LowerCorner.z = fZ0;
+	cBBox.f3UpperCorner.x = fX1;
+	cBBox.f3UpperCorner.y = fY1;
+	cBBox.f3UpperCorner.z = fZ1;
+}
+
+void
+CClipWin::_DisplayFunc()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// plot the bounding box
+	glPushMatrix();
+	glTranslatef(
+		cBBox.f3LowerCorner.x,
+		cBBox.f3LowerCorner.y,
+		cBBox.f3LowerCorner.z);
+	glScalef(
+		(cBBox.f3UpperCorner.x - cBBox.f3LowerCorner.x)/2.0f,
+		(cBBox.f3UpperCorner.y - cBBox.f3LowerCorner.y)/2.0f,
+		(cBBox.f3UpperCorner.z - cBBox.f3LowerCorner.z)/2.0f);
+	glTranslatef(1.0f, 1.0f, 1.0f);
+
+	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+	glutWireCube(2.0f);
+	glPopMatrix();
+
+	glPushAttrib(GL_ENABLE_BIT);
+	glDisable(GL_CULL_FACE);
+	glDisable(GL_DEPTH_TEST);
+
+								// step-2:
+								// plot the clipping planes
+	if( ibPlotClippingPlanes )
+	{
+		glPushAttrib(GL_POLYGON_BIT);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+#if	1	
+		glBegin(GL_QUADS);
+		for(int cp = 0; cp < CClipPlanes::NR_OF_CLIP_PLANES; cp++) 
+		{
+			if( cClipPlanes.piFlags[cp] )
+			{
+				// find the primal direction
+				int iPrimalDir = 0;
+				for(int j = 1; j < 3; j++)
+					if( fabs(cClipPlanes.ppdPlanes[cp][iPrimalDir]) < fabs(cClipPlanes.ppdPlanes[cp][j]) )
+						iPrimalDir = j;
+
+				glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+				switch(iPrimalDir)
+				{
+				case 0:
+					{
+					float fX;
+					fX = -(cClipPlanes.ppdPlanes[cp][3] + cClipPlanes.ppdPlanes[cp][1] * cBBox.f3LowerCorner.y + cClipPlanes.ppdPlanes[cp][2] * cBBox.f3LowerCorner.z)/cClipPlanes.ppdPlanes[cp][iPrimalDir];
+					glVertex3f(fX, cBBox.f3LowerCorner.y, cBBox.f3LowerCorner.z);
+
+					fX = -(cClipPlanes.ppdPlanes[cp][3] + cClipPlanes.ppdPlanes[cp][1] * cBBox.f3UpperCorner.y + cClipPlanes.ppdPlanes[cp][2] * cBBox.f3LowerCorner.z)/cClipPlanes.ppdPlanes[cp][iPrimalDir];
+					glVertex3f(fX, cBBox.f3UpperCorner.y, cBBox.f3LowerCorner.z);
+
+					fX = -(cClipPlanes.ppdPlanes[cp][3] + cClipPlanes.ppdPlanes[cp][1] * cBBox.f3UpperCorner.y + cClipPlanes.ppdPlanes[cp][2] * cBBox.f3UpperCorner.z)/cClipPlanes.ppdPlanes[cp][iPrimalDir];
+					glVertex3f(fX, cBBox.f3UpperCorner.y, cBBox.f3UpperCorner.z);
+
+					fX = -(cClipPlanes.ppdPlanes[cp][3] + cClipPlanes.ppdPlanes[cp][1] * cBBox.f3LowerCorner.y + cClipPlanes.ppdPlanes[cp][2] * cBBox.f3UpperCorner.z)/cClipPlanes.ppdPlanes[cp][iPrimalDir];
+					glVertex3f(fX, cBBox.f3LowerCorner.y, cBBox.f3UpperCorner.z);
+					}
+
+					break;
+
+				case 1:
+					{
+					float fY;
+					fY = -(cClipPlanes.ppdPlanes[cp][3] + cClipPlanes.ppdPlanes[cp][0] * cBBox.f3LowerCorner.x + cClipPlanes.ppdPlanes[cp][2] * cBBox.f3LowerCorner.z)/cClipPlanes.ppdPlanes[cp][iPrimalDir];
+					glVertex3f(cBBox.f3LowerCorner.x, fY, cBBox.f3LowerCorner.z);
+
+					fY = -(cClipPlanes.ppdPlanes[cp][3] + cClipPlanes.ppdPlanes[cp][0] * cBBox.f3UpperCorner.x + cClipPlanes.ppdPlanes[cp][2] * cBBox.f3LowerCorner.z)/cClipPlanes.ppdPlanes[cp][iPrimalDir];
+					glVertex3f(cBBox.f3UpperCorner.x, fY, cBBox.f3LowerCorner.z);
+
+					fY = -(cClipPlanes.ppdPlanes[cp][3] + cClipPlanes.ppdPlanes[cp][0] * cBBox.f3UpperCorner.x + cClipPlanes.ppdPlanes[cp][2] * cBBox.f3UpperCorner.z)/cClipPlanes.ppdPlanes[cp][iPrimalDir];
+					glVertex3f(cBBox.f3UpperCorner.x, fY, cBBox.f3UpperCorner.z);
+
+					fY = -(cClipPlanes.ppdPlanes[cp][3] + cClipPlanes.ppdPlanes[cp][0] * cBBox.f3LowerCorner.x + cClipPlanes.ppdPlanes[cp][2] * cBBox.f3UpperCorner.z)/cClipPlanes.ppdPlanes[cp][iPrimalDir];
+					glVertex3f(cBBox.f3LowerCorner.x, fY, cBBox.f3UpperCorner.z);
+					}
+
+					break;
+
+				case 2:
+					{
+					float fZ;
+					fZ = -(cClipPlanes.ppdPlanes[cp][3] + cClipPlanes.ppdPlanes[cp][0] * cBBox.f3LowerCorner.x + cClipPlanes.ppdPlanes[cp][1] * cBBox.f3LowerCorner.y)/cClipPlanes.ppdPlanes[cp][iPrimalDir];
+					glVertex3f(cBBox.f3LowerCorner.x, cBBox.f3LowerCorner.y, fZ);
+
+					fZ = -(cClipPlanes.ppdPlanes[cp][3] + cClipPlanes.ppdPlanes[cp][0] * cBBox.f3UpperCorner.x + cClipPlanes.ppdPlanes[cp][1] * cBBox.f3LowerCorner.y)/cClipPlanes.ppdPlanes[cp][iPrimalDir];
+					glVertex3f(cBBox.f3UpperCorner.x, cBBox.f3LowerCorner.y, fZ);
+
+					fZ = -(cClipPlanes.ppdPlanes[cp][3] + cClipPlanes.ppdPlanes[cp][0] * cBBox.f3UpperCorner.x + cClipPlanes.ppdPlanes[cp][1] * cBBox.f3UpperCorner.y)/cClipPlanes.ppdPlanes[cp][iPrimalDir];
+					glVertex3f(cBBox.f3UpperCorner.x, cBBox.f3UpperCorner.y, fZ);
+
+					fZ = -(cClipPlanes.ppdPlanes[cp][3] + cClipPlanes.ppdPlanes[cp][0] * cBBox.f3LowerCorner.x + cClipPlanes.ppdPlanes[cp][1] * cBBox.f3UpperCorner.y)/cClipPlanes.ppdPlanes[cp][iPrimalDir];
+					glVertex3f(cBBox.f3LowerCorner.x, cBBox.f3UpperCorner.y, fZ);
+					}
+
+					break;
+				}
+			}
+		}
+#else
+		glBegin(GL_TRIANGLES);
+		for(int cp = 0; cp < CClipPlanes::NR_OF_CLIP_PLANES; cp++) 
+		{
+			if( cClipPlanes.piFlags[cp] )
+			{
+				glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+
+				float pfVertex[3];
+				for(int a = 0; a < 3; a++)
+				{
+					memset(pfVertex, 0, sizeof(pfVertex));
+					if( cClipPlanes.ppdPlanes[cp][a] != 0.0 )
+					{
+						pfVertex[a] = -(cClipPlanes.ppdPlanes[cp][3])/cClipPlanes.ppdPlanes[cp][a];
+					}
+					glVertex3fv(pfVertex);
+				}
+			}
+		}
+#endif
+		glEnd();
+		glPopAttrib();	// glPushAttrib(GL_POLYGON_BIT);
+	}	//	if( ibPlotClippingPlanes )
+
+								// step-3:
+								// plot the interior volume
+	if( cInteriorVolume.ibIsEnabled )
+	{
+		
+									// step-1:
+									// setup clipping planes
+		for(int i=0; i < CClipPlanes::NR_OF_CLIP_PLANES; i++) 
+		{
+			if( cClipPlanes.piFlags[i] )
+			{
+				glEnable(GL_CLIP_PLANE0 + i);
+				glClipPlane(GL_CLIP_PLANE0 + i, cClipPlanes.ppdPlanes[i]);
+			}
+		}
+
+		glEnable( GL_BLEND);
+
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+
+		int iNrOfSlices = 16;
+
+		glColor4f(1.0f, 1.0f, 1.0f, cInteriorVolume.fOpacity / (3.0f * (float)iNrOfSlices));
+
+		glPushMatrix();
+		glTranslatef(
+			cBBox.f3LowerCorner.x,
+			cBBox.f3LowerCorner.y,
+			cBBox.f3LowerCorner.z);
+		glScalef(
+			(cBBox.f3UpperCorner.x - cBBox.f3LowerCorner.x)/2.0f,
+			(cBBox.f3UpperCorner.y - cBBox.f3LowerCorner.y)/2.0f,
+			(cBBox.f3UpperCorner.z - cBBox.f3LowerCorner.z)/2.0f);
+		glTranslatef(1.0f, 1.0f, 1.0f);
+
+		static GLuint lidInteriorVolume;
+
+		if( !lidInteriorVolume )
+		{
+			lidInteriorVolume = glGenLists(1);
+			glNewList(lidInteriorVolume, GL_COMPILE_AND_EXECUTE);
+			for(int i = 0; i < 3; i++)
+			{
+				glPushMatrix();
+				switch(i)
+				{
+				case 0:		glRotatef(90.0f, 0.0f, 0.0f, 1.0f);	break;
+				case 1:		glRotatef(90.0f, 1.0f, 0.0f, 0.0f);	break;
+				}
+				glBegin(GL_QUADS);
+				for(int z=0; z <= iNrOfSlices; z++)
+				{
+					double dZ = 2.0 * (double)z / (double)iNrOfSlices - 1.0; 
+
+					glVertex3d(-1, -1, dZ);
+					glVertex3d(+1, -1, dZ);
+					glVertex3d(+1, +1, dZ);
+					glVertex3d(-1, +1, dZ);
+				}
+				glEnd();
+				glPopMatrix();
+			}
+			glEndList();
+		}
+		else
+		{
+			glCallList(lidInteriorVolume);
+		}
+		glPopMatrix();
+	}	// if( ibPlotInteriorVolume )
+
+	glPopAttrib();	// glPushAttrib(GL_ENABLE_BIT);	
+
+	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+	glBegin(GL_LINES);
+	glVertex3i(0, 0, 0);
+	glVertex3i(1, 0, 0);
+	glEnd();
+	_DrawString3D("X", 1.0f, 0.0f, 0.0f);
+
+	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
+	glBegin(GL_LINES);
+	glVertex3i(0, 0, 0);
+	glVertex3i(0, 1, 0);
+	glEnd();
+	_DrawString3D("Y", 0.0f, 1.0f, 0.0f);
+
+	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+	glBegin(GL_LINES);
+	glVertex3i(0, 0, 0);
+	glVertex3i(0, 0, 1);
+	glEnd();
+	_DrawString3D("Z", 0.0f, 0.0f, 1.0f);
+}
+
+void
+CClipWin::_InitFunc()
+{
+		// keep updating the display window to reflect the modification from the window CClipEditor
+	_KeepUpdateOn();
+
+	PCGetGluiSubwin()->add_checkbox("Plot Clipping Planes?", &ibPlotClippingPlanes);
+	PCGetGluiSubwin()->add_checkbox("Keep Update?", &ibKeepUpdate);
+	PCGetGluiSubwin()->add_checkbox("Keep Update?", &ibDisableAll);
+	PCGetGluiSubwin()->add_button("Update", IAddWid(EVENT_BUTTON_UPDATE), &CGlutWin::_GluiCB_static);
+
+	PCGetGluiSubwin()->add_column();
+	cInteriorVolume._AddGlui(PCGetGluiSubwin());
+
+
+	cClipEditor._SetClipPlanes(&cClipPlanes);
+	cClipEditor.ICreate("Clip Plane Editor", false, 100, 100, 256, 384);
+
+	_Set();
+
+	_Reshape(384, 384);
+	
+}
+
+void
+CClipWin::_PassEnabledClipPlanes()
+{
+	static double ppdEnalbedClipPlanes[CClipPlanes::NR_OF_CLIP_PLANES][4];
+	int iNrOfEnabledClipPlanes = 0;
+
+	if( 0 == ibDisableAll )
+		for(int cp = 0; cp < CClipPlanes::NR_OF_CLIP_PLANES; cp++)
+			if( cClipPlanes.piFlags[cp] )
+			{
+				memcpy(ppdEnalbedClipPlanes[iNrOfEnabledClipPlanes], cClipPlanes.ppdPlanes[cp], sizeof(ppdEnalbedClipPlanes[0]));
+				iNrOfEnabledClipPlanes++;
+			}
+
+	CGlutWin::_GlobalCB(
+		IGetId(), 
+		CGlutWin::CB_MANUAL, 
+		EVENT_BUTTON_UPDATE, 
+		iNrOfEnabledClipPlanes,		// #enabled clipping planes
+		(double**)ppdEnalbedClipPlanes);		// pointer to the array of enabled clipping planes
+}
+
+void 
+CClipWin::_IdleFunc()
+{
+	if( ibKeepUpdate )
+		_PassEnabledClipPlanes();
+}
+
+
+void 
+CClipWin::_GluiFunc(unsigned short usValue)
+{
+	switch(usValue)
+	{
+	case EVENT_BUTTON_UPDATE:
+		_PassEnabledClipPlanes();
+		break;
+	}
+}
+
+CClipWin::CClipWin(void)
+{
+	_AddGluiSubwin(GLUI_SUBWINDOW_TOP);
+
+	cBBox.f3LowerCorner.x = -1.0f;
+	cBBox.f3LowerCorner.y = -1.0f;
+	cBBox.f3LowerCorner.z = -1.0f;
+	cBBox.f3UpperCorner.x =  1.0f;
+	cBBox.f3UpperCorner.y =  1.0f;
+	cBBox.f3UpperCorner.z =  1.0f;
+}
+
+CClipWin::~CClipWin(void)
+{
+}
+
+/*
+
+$Log: not supported by cvs2svn $
+
+*/
---
+++ trunk/libclip/ClipWin_ClipEditor.cpp
@@ -0,0 +1,220 @@
+#include "lib3ds/vector.h"
+
+#include "ClipWin.h"
+
+#include <iostream>
+#include "liblog.h"
+
+void 
+CClipWin::CClipEditor::_DisplayFunc()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glPushMatrix();
+	glLoadMatrixd(this->tViewMatrix);
+
+	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+	glBegin(GL_LINES);
+	glVertex3i(0, 0, 0);
+	glVertex3i(1, 0, 0);
+	glEnd();
+	_DrawString3D("X", 1.0f, 0.0f, 0.0f);
+
+	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
+	glBegin(GL_LINES);
+	glVertex3i(0, 0, 0);
+	glVertex3i(0, 1, 0);
+	glEnd();
+	_DrawString3D("Y", 0.0f, 1.0f, 0.0f);
+
+	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+	glBegin(GL_LINES);
+	glVertex3i(0, 0, 0);
+	glVertex3i(0, 0, 1);
+	glEnd();
+	_DrawString3D("Z", 0.0f, 0.0f, 1.0f);
+
+	glPopMatrix();
+
+	glPushAttrib(GL_ENABLE_BIT);
+	glPushAttrib(GL_POLYGON_BIT);
+
+	Lib3dsVector v3Normal;
+	v3Normal[0] = f4ClipPlane.x;
+	v3Normal[1] = f4ClipPlane.y;
+	v3Normal[2] = f4ClipPlane.z;
+	float fNormalLength = lib3ds_vector_length(v3Normal);
+
+	if( 0.0f < fNormalLength )
+	{
+		glPolygonMode(GL_BACK, GL_LINE);
+
+		glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+		glBegin(GL_QUADS);
+		glVertex2i(-1, -1);
+		glVertex2i(+1, -1);
+		glVertex2i(+1, +1);
+		glVertex2i(-1, +1);
+		glEnd();
+
+		glLineWidth(1.0f);
+		glColor4f(0.5f, 0.5f, 0.5f, 1.0f);
+		glBegin(GL_LINES);
+		glVertex3f(0.0f, 0.0f, 0.0f);
+		glVertex3f(0.0f, 0.0f, fNormalLength);
+		glEnd();
+	}
+	glPopAttrib();	//	glPushAttrib(GL_ENABLE_BIT);
+	glPopAttrib();	//	glPushAttrib(GL_POLYGON_BIT);
+}
+
+void 
+CClipWin::CClipEditor::_InitFunc()
+{
+	assert(pcClipPlanes);
+
+	_KeepUpdateOn();
+
+	iFlag = pcClipPlanes->piFlags[iActiveClipPlaneId];
+	f4ClipPlane.x = pcClipPlanes->ppdPlanes[iActiveClipPlaneId][0];
+	f4ClipPlane.y = pcClipPlanes->ppdPlanes[iActiveClipPlaneId][1];
+	f4ClipPlane.z = pcClipPlanes->ppdPlanes[iActiveClipPlaneId][2];
+	f4ClipPlane.w = pcClipPlanes->ppdPlanes[iActiveClipPlaneId][3];
+
+	GLUI_Listbox *pcListbox;
+	pcListbox = PCGetGluiSubwin()->add_listbox("Plane",		&iActiveClipPlaneId,		IAddWid(EVENT_LIST_CLIP_PLANE), &CGlutWin::_GluiCB_static);
+	for(int i = 0; i < CClipPlanes::NR_OF_CLIP_PLANES; i++)
+		pcListbox->add_item(i, SZSprintf("%d", i));
+
+	PCGetGluiSubwin()->add_checkbox("Enabled?", &iFlag, IAddWid(EVENT_CHECKBOX_ENABLE), &CGlutWin::_GluiCB_static);
+	PCGetGluiSubwin()->add_column();
+
+	PCGetGluiSubwin()->add_spinner("X", GLUI_SPINNER_FLOAT, &f4ClipPlane.x,	IAddWid(EVENT_SPINNER_PLANE_X), &CGlutWin::_GluiCB_static);
+	PCGetGluiSubwin()->add_spinner("Y", GLUI_SPINNER_FLOAT, &f4ClipPlane.y,	IAddWid(EVENT_SPINNER_PLANE_Y), &CGlutWin::_GluiCB_static);
+	PCGetGluiSubwin()->add_spinner("Z", GLUI_SPINNER_FLOAT, &f4ClipPlane.z,	IAddWid(EVENT_SPINNER_PLANE_Z), &CGlutWin::_GluiCB_static);
+	PCGetGluiSubwin()->add_spinner("W", GLUI_SPINNER_FLOAT, &f4ClipPlane.w,	IAddWid(EVENT_SPINNER_PLANE_W), &CGlutWin::_GluiCB_static);
+
+	_Reshape(384, 384);
+}
+
+void 
+CClipWin::CClipEditor::_GluiFunc(unsigned short usValue)
+{
+	switch(usValue)
+	{
+	case EVENT_LIST_CLIP_PLANE:
+		memcpy(tModelMatrix, pmMatrices[iActiveClipPlaneId], sizeof(tModelMatrix));
+
+		iFlag = pcClipPlanes->piFlags[iActiveClipPlaneId];
+		f4ClipPlane.x = pcClipPlanes->ppdPlanes[iActiveClipPlaneId][0];
+		f4ClipPlane.y = pcClipPlanes->ppdPlanes[iActiveClipPlaneId][1];
+		f4ClipPlane.z = pcClipPlanes->ppdPlanes[iActiveClipPlaneId][2];
+		f4ClipPlane.w = pcClipPlanes->ppdPlanes[iActiveClipPlaneId][3];
+		break;
+
+	case EVENT_SPINNER_PLANE_X:
+	case EVENT_SPINNER_PLANE_Y:		
+	case EVENT_SPINNER_PLANE_Z:
+		{
+			Lib3dsVector v3Z;
+			v3Z[0] = 0.0f; 
+			v3Z[1] = 0.0f; 
+			v3Z[2] = 1.0f;
+		
+			Lib3dsVector v3Normal;
+			v3Normal[0] = f4ClipPlane.x;
+			v3Normal[1] = f4ClipPlane.y;
+			v3Normal[2] = f4ClipPlane.z;
+			lib3ds_vector_normalize(v3Normal);
+
+			Lib3dsVector v3Up;
+			lib3ds_vector_cross(v3Up, v3Z, v3Normal);
+			lib3ds_vector_normalize(v3Up);
+
+			float fCos = lib3ds_vector_dot(v3Z, v3Normal);
+			float fAngle_degree = 180.0f * acos(fCos) / M_PI;
+
+			glPushMatrix();
+			glLoadIdentity();
+ 
+			float3 f3Translation;
+			memset(&f3Translation, 0, sizeof(f3Translation));
+			switch(usValue)
+			{
+			case EVENT_SPINNER_PLANE_X:	if(f4ClipPlane.x) f3Translation.x = -f4ClipPlane.w / f4ClipPlane.x;	break;
+			case EVENT_SPINNER_PLANE_Y:	if(f4ClipPlane.y) f3Translation.y = -f4ClipPlane.w / f4ClipPlane.y;	break;	
+			case EVENT_SPINNER_PLANE_Z:	if(f4ClipPlane.z) f3Translation.z = -f4ClipPlane.w / f4ClipPlane.z;	break;
+			}
+
+			glTranslatef(f3Translation.x, f3Translation.y, f3Translation.z);
+			glRotatef(fAngle_degree, v3Up[0], v3Up[1], v3Up[2]);
+			glTranslatef(-f3Translation.x, -f3Translation.y, -f3Translation.z);
+
+			glGetDoublev(GL_MODELVIEW_MATRIX, tModelMatrix);
+
+			glPopMatrix();
+		}
+		break;
+	case EVENT_SPINNER_PLANE_W:
+		{
+			// give the plane equation ax + by + cz + d = 0, calculate the current d
+			float3 f3Origin;
+			f3Origin.x = tModelMatrix[12];
+			f3Origin.y = tModelMatrix[13];
+			f3Origin.z = tModelMatrix[14];
+			float fD = -(f4ClipPlane.x * f3Origin.x + f4ClipPlane.y * f3Origin.y + f4ClipPlane.z * f3Origin.z);
+			float fT = (fD - f4ClipPlane.w) / (f4ClipPlane.x * f4ClipPlane.x + f4ClipPlane.y * f4ClipPlane.y + f4ClipPlane.z * f4ClipPlane.z);
+			tModelMatrix[12] += fT * f4ClipPlane.x; 
+			tModelMatrix[13] += fT * f4ClipPlane.y; 
+			tModelMatrix[14] += fT * f4ClipPlane.z; 
+		}
+		break;
+	}
+}
+
+void 
+CClipWin::CClipEditor::_IdleFunc()
+{
+	f4ClipPlane.x = (float)tModelMatrix[8];
+	f4ClipPlane.y = (float)tModelMatrix[9];
+	f4ClipPlane.z = (float)tModelMatrix[10];
+	f4ClipPlane.w = -(float)(f4ClipPlane.x * tModelMatrix[12] + f4ClipPlane.y * tModelMatrix[13] + f4ClipPlane.z * tModelMatrix[14]);
+
+	pcClipPlanes->piFlags[iActiveClipPlaneId] = iFlag;
+	pcClipPlanes->ppdPlanes[iActiveClipPlaneId][0] = f4ClipPlane.x;
+	pcClipPlanes->ppdPlanes[iActiveClipPlaneId][1] = f4ClipPlane.y;
+	pcClipPlanes->ppdPlanes[iActiveClipPlaneId][2] = f4ClipPlane.z;
+	pcClipPlanes->ppdPlanes[iActiveClipPlaneId][3] = f4ClipPlane.w;
+
+	memcpy(pmMatrices[iActiveClipPlaneId], tModelMatrix, sizeof(tModelMatrix));
+}
+
+CClipWin::CClipEditor::CClipEditor()
+{
+	iActiveClipPlaneId = 0;
+	pcClipPlanes = NULL;
+	_AddGluiSubwin(GLUI_SUBWINDOW_TOP);
+
+	static TMatrix mIdentity = {
+		1.0, 0.0, 0.0, 0.0,
+		0.0, 1.0, 0.0, 0.0,
+		0.0, 0.0, 1.0, 0.0,
+		0.0, 0.0, 0.0, 1.0,
+	};
+
+	for(int cp = 0; cp < CClipPlanes::NR_OF_CLIP_PLANES; cp++)
+	{
+		memcpy(pmMatrices[cp], mIdentity, sizeof(TMatrix));
+	}
+}
+
+
+CClipWin::CClipEditor::~CClipEditor()
+{
+}
+
+/*
+
+$Log: not supported by cvs2svn $
+
+*/
