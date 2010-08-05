1a255823907f41a4725cf2b6de029200d2023049
leeten <leeten@abdb6057-6f73-fb73-b203-3162f873fe11>
2010-08-05 19:21:29 +0000

    [08/05/2010] 1. [DEL] Remove the linkage of libgw.lib.

--- libgw/examples/example_multi_wins_global/example_multi_wins_global.cpp
+++ libgw/examples/example_multi_wins_global/example_multi_wins_global.cpp
@@ -7,7 +7,9 @@ to create an application with three GLUT windows.
 
 **************************************************/
 
-#pragma comment (lib, "libgw.lib")      // link with the library libgw
+	// DEL-BY-LEETEN 08/05/2010-BEGIN
+		// #pragma comment (lib, "libgw.lib")      // link with the library libgw
+	// DEL-BY-LEETEN 08/05/2010-END
 
 #include "../include/TestWin.h"
 
@@ -53,6 +55,11 @@ main(int argn, char *argv[])
 /*
 
 $Log: not supported by cvs2svn $
+Revision 1.2  2009/06/01 21:40:00  leeten
+
+[2009/06/01]
+1. [MOD] Change the path of TestWin.h to ../include/TestWin.h.
+
 Revision 1.1  2009/01/16 14:37:00  leeten
 
 [2009/01/16]
