f3a2d463ed3fcf6399b9ec126ed26794340b91fa
leeten <leeten@abdb6057-6f73-fb73-b203-3162f873fe11>
2011-01-24 02:53:49 +0000

    [01/23/2011] 1. [MOD] Use the type double to store the time. 2. [ADD] Use the function clock_gettime in non-Win32 config.

--- libfps/FPS_COUNTER.cpp
+++ libfps/FPS_COUNTER.cpp
@@ -6,6 +6,8 @@
 //	Downloaded from: www.paulsprojects.net
 //	Created:	20th July 2002
 //////////////////////////////////////////////////////////////////////////////////////////
+
+#if	0	// DEL-BY-LEETEN 01/23/2011-BEGIN
 	// ADD-BY-LEETEN-2009/06/01-BEGIN
 	#ifdef WIN32	
 	// ADD-BY-LEETEN-2009/06/01-END
@@ -17,31 +19,43 @@
 	#include <time.h>
 	#endif
 	// ADD-BY-LEETEN-2009/06/01-END
+#endif	// DEL-BY-LEETEN 01/23/2011-END
 
 #include "FPS_COUNTER.h"
 
 void FPS_COUNTER::Update(void)
 {
 	//keep track of time passed and frame count
+	#if	0	// MOD-BY-LEETEN 01/23/2011-FROM:
+		// ADD-BY-LEETEN-2009/06/01-BEGIN
+		#ifdef WIN32
+		// ADD-BY-LEETEN-2009/06/01-END
 
-	// ADD-BY-LEETEN-2009/06/01-BEGIN
+		time=timeGetTime()*0.001f;
+
+		// ADD-BY-LEETEN-2009/06/01-BEGIN
+		#else
+		time=(float)clock() * 0.001;
+		#endif
+		// ADD-BY-LEETEN-2009/06/01-END
+		++frames;
+
+		//If a second has passed
+		if(time-lastTime>1.0f)
+		{
+			fps=frames/(time-lastTime);	//update the number of frames per second
+			lastTime=time;				//set time for the start of the next count
+			frames=0;					//reset fps for this second
+		}
+	#else	// MOD-BY-LEETEN 01/23/2011-TO:
 	#ifdef WIN32
-	// ADD-BY-LEETEN-2009/06/01-END
-
-	time=timeGetTime()*0.001f;
-
-	// ADD-BY-LEETEN-2009/06/01-BEGIN
+	time = timeGetTime()/1000.0;
 	#else
-	time=(float)clock() * 0.001;
+	struct timespec cTime;
+	clock_gettime(CLOCK_REALTIME, &cTime)
+	time = (double)cTime.tv_sec + (double)cTime.tv_nsec/1.0e+9;
 	#endif
-	// ADD-BY-LEETEN-2009/06/01-END
-	++frames;
-
-	//If a second has passed
-	if(time-lastTime>1.0f)
-	{
-		fps=frames/(time-lastTime);	//update the number of frames per second
-		lastTime=time;				//set time for the start of the next count
-		frames=0;					//reset fps for this second
-	}
+	fps = (float)(1.0 / (time-lastTime));
+	lastTime=time;				//set time for the start of the next count
+	#endif		// MOD-BY-LEETEN 01/23/2011-END
 }
