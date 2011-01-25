95ba12e64c07fbcb454270c6f4e862948803b538
leeten <leeten@abdb6057-6f73-fb73-b203-3162f873fe11>
2011-01-25 06:19:17 +0000

    [01/25/2011] 1. [MOD] Add the type declarere <t> when call the class TBuffer or access its member,

--- libbuf/libbuf2d.h
+++ libbuf/libbuf2d.h
@@ -19,7 +19,11 @@ struct TBuffer2D: public TBuffer<t>
 	int iWidth;
 	int iHeight;
 
-	TBuffer2D():TBuffer()
+	// MOD-BY-LEETEN 01/25/2011-FROM:
+		// TBuffer2D():TBuffer()
+	// TO:
+	TBuffer2D():TBuffer<t>()
+	// MOD-BY-LEETEN 01/25/2011-END
 	{
 		iWidth = 0;
 		iHeight = 0;
@@ -32,7 +36,11 @@ struct TBuffer2D: public TBuffer<t>
 	// ADD-BY-LEETEN 01/26/2010-BEGIN
 	t* alloc(int n)
 	{
-		TBuffer::alloc(n);
+		// MOD-BY-LEETEN 01/25/2011-FROM:
+			// TBuffer::alloc(n);
+		// TO:
+		TBuffer<t>::alloc(n);
+		// MOD-BY-LEETEN 01/25/2011-END
 		return data;
 	}
 	// ADD-BY-LEETEN 01/26/2010-END
@@ -42,28 +50,45 @@ struct TBuffer2D: public TBuffer<t>
 		assert(w > 0 && h > 0);
 		iWidth = w;
 		iHeight = h;
-		TBuffer::alloc(iWidth * iHeight);
-		return data;
+		#if	0	// MOD-BY-LEETEN 01/25/2011-FROM:
+			TBuffer::alloc(iWidth * iHeight);
+			return data;
+		#else	// MOD-BY-LEETEN 01/25/2011-TO:
+		TBuffer<t>::alloc(iWidth * iHeight);
+		return TBuffer<t>::data;
+		#endif	// MOD-BY-LEETEN 01/25/2011-END
 	}
 
 	t& GetElement(int x, int y)
 	{
 		assert( 0 <= x && x < iWidth);
 		assert( 0 <= y && y < iHeight);
-		return data[x + y * iWidth];
+		// MOD-BY-LEETEN 01/25/2011-FROM:
+			// return data[x + y * iWidth];
+		// TO:
+		return TBuffer<t>::data[x + y * iWidth];
+		// MOD-BY-LEETEN 01/25/2011-END
 	}
 
 	const t& GetElement(int x, int y) const
 	{
 		assert( 0 <= x && x < iWidth);
 		assert( 0 <= y && y < iHeight);
-		return data[x + y * iWidth];
+		// MOD-BY-LEETEN 01/25/2011-FROM:
+			// return data[x + y * iWidth];
+		// TO:
+		return TBuffer<t>::data[x + y * iWidth];
+		// MOD-BY-LEETEN 01/25/2011-END
 	}
 
 	// ADD-BY-LEETEN 09/19/2010-BEGIN
 	void _Save(char *szFilenamePrefix)
 	{
-		if( !BIsAllocated() )
+		// MOD-BY-LEETEN 01/25/2011-FROM:
+			// if( !BIsAllocated() )
+		// TO:
+		if( !TBuffer<t>::BIsAllocated() )
+		// MOD-BY-LEETEN 01/25/2011-END
 		{
 			LOG("Non-initialized buffer");
 			return;
@@ -82,7 +107,11 @@ struct TBuffer2D: public TBuffer<t>
 
 		fwrite(&iWidth, sizeof(iWidth), 1, fp);
 		fwrite(&iHeight, sizeof(iHeight), 1, fp);
-		fwrite(data, sizeof(t), num, fp);
+		// MOD-BY-LEETEN 01/25/2011-FROM:
+			// fwrite(data, sizeof(t), num, fp);
+		// TO:
+		fwrite(TBuffer<t>::data, sizeof(t), TBuffer<t>::num, fp);
+		// MOD-BY-LEETEN 01/25/2011-END
 
 		fclose(fp);
 	}
@@ -94,6 +123,12 @@ struct TBuffer2D: public TBuffer<t>
 /*
 
 $Log: not supported by cvs2svn $
+Revision 1.3  2010/09/21 13:29:15  leeten
+
+[09/21/2010]
+1. [ADD] Define a new method _Save() to dump the data in the buffer.
+2. [ADD] Define a new macro SAVE_BUF to dump the data into a file which is automatically named according to the variable's name and scope.
+
 Revision 1.2  2010/01/29 19:57:00  leeten
 
 [01/29/2010]
