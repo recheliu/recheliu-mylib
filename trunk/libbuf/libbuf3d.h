c61625d813348d3654c325f745778dbc05c7cc36
leeten <leeten@abdb6057-6f73-fb73-b203-3162f873fe11>
2011-01-25 06:19:17 +0000

    [01/25/2011] 1. [MOD] Add the type declarere <t> when call the class TBuffer or access its member,

--- trunk/libbuf/libbuf3d.h
+++ trunk/libbuf/libbuf3d.h
@@ -20,7 +20,11 @@ struct TBuffer3D: public TBuffer<t>
 	int iHeight;
 	int iDepth;
 
-	TBuffer3D():TBuffer()
+	// MOD-BY-LEETEN 01/25/2011-FROM:
+		// TBuffer3D():TBuffer()
+	// TO:
+	TBuffer3D():TBuffer<t>()
+	// MOD-BY-LEETEN 01/25/2011-END
 	{
 		iWidth = 0;
 		iHeight = 0;
@@ -34,8 +38,13 @@ struct TBuffer3D: public TBuffer<t>
 	// ADD-BY-LEETEN 01/26/2010-BEGIN
 	t* alloc(int n)
 	{
-		TBuffer::alloc(n);
-		return data;
+		#if	0	// MOD-BY-LEETEN 01/25/2011-FROM:
+			TBuffer::alloc(n);
+			return data;
+		#else	// MOD-BY-LEETEN 01/25/2011-TO:
+		TBuffer<t>::alloc(n);
+		return TBuffer<t>::data;
+		#endif	// MOD-BY-LEETEN 01/25/2011-END
 	}
 	// ADD-BY-LEETEN 01/26/2010-END
 
@@ -45,8 +54,13 @@ struct TBuffer3D: public TBuffer<t>
 		iWidth = w;
 		iHeight = h;
 		iDepth = d;
-		TBuffer::alloc(iWidth * iHeight * iDepth);
-		return data;
+		#if	0	// MOD-BY-LEETEN 01/25/2011-FROM:
+			TBuffer::alloc(iWidth * iHeight * iDepth);
+			return data;
+		#else	// MOD-BY-LEETEN 01/25/2011-TO:
+		TBuffer<t>::alloc(iWidth * iHeight * iDepth);
+		return TBuffer<t>::data;
+		#endif	// MOD-BY-LEETEN 01/25/2011-END
 	}
 
 	t& GetElement(int x, int y, int z)
@@ -54,7 +68,11 @@ struct TBuffer3D: public TBuffer<t>
 		assert( 0 <= x && x < iWidth);
 		assert( 0 <= y && y < iHeight);
 		assert( 0 <= z && z < iDepth);
-		return data[x + y * iWidth + z * iWidth * iHeight];
+		// MOD-BY-LEETEN 01/25/2011-FROM:
+			// return data[x + y * iWidth + z * iWidth * iHeight];
+		// TO:
+		return TBuffer<t>::data[x + y * iWidth + z * iWidth * iHeight];
+		// MOD-BY-LEETEN 01/25/2011-END
 	}
 
 	const t& GetElement(int x, int y, int z) const
@@ -62,13 +80,21 @@ struct TBuffer3D: public TBuffer<t>
 		assert( 0 <= x && x < iWidth);
 		assert( 0 <= y && y < iHeight);
 		assert( 0 <= z && z < iDepth);
-		return data[x + y * iWidth + z * iWidth * iHeight];
+		// MOD-BY-LEETEN 01/25/2011-FROM:
+			// return data[x + y * iWidth + z * iWidth * iHeight];
+		// TO:
+		return TBuffer<t>::data[x + y * iWidth + z * iWidth * iHeight];
+		// MOD-BY-LEETEN 01/25/2011-END
 	}
 
 	// ADD-BY-LEETEN 09/19/2010-BEGIN
 	void _Save(char *szFilenamePrefix)
 	{
-		if( !BIsAllocated() )
+		// MOD-BY-LEETEN 01/25/2011-FROM:
+			// if( !BIsAllocated() )
+		// TO:
+		if( !TBuffer<t>::BIsAllocated() )
+		// MOD-BY-LEETEN 01/25/2011-END
 		{
 			LOG("Non-initialized buffer");
 			return;
@@ -88,7 +114,11 @@ struct TBuffer3D: public TBuffer<t>
 		fwrite(&iWidth, sizeof(iWidth), 1, fp);
 		fwrite(&iHeight, sizeof(iHeight), 1, fp);
 		fwrite(&iDepth, sizeof(iDepth), 1, fp);
-		fwrite(data, sizeof(t), num, fp);
+		// MOD-BY-LEETEN 01/25/2011-FROM:
+			// fwrite(data, sizeof(t), num, fp);
+		// TO:
+		fwrite(TBuffer<t>::data, sizeof(t), TBuffer<t>::num, fp);
+		// MOD-BY-LEETEN 01/25/2011-END
 
 		fclose(fp);
 	}
@@ -100,6 +130,12 @@ struct TBuffer3D: public TBuffer<t>
 /*
 
 $Log: not supported by cvs2svn $
+Revision 1.3  2010/09/21 13:29:15  leeten
+
+[09/21/2010]
+1. [ADD] Define a new method _Save() to dump the data in the buffer.
+2. [ADD] Define a new macro SAVE_BUF to dump the data into a file which is automatically named according to the variable's name and scope.
+
 Revision 1.2  2010/01/29 19:57:00  leeten
 
 [01/29/2010]
