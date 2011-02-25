38e7b59438a3ffbec1050568fed49714699fadca
leeten <leeten@abdb6057-6f73-fb73-b203-3162f873fe11>
2011-02-25 02:30:08 +0000

    [02/20/2011] 1. [MOD] Delay the printing of header until LIBCLOCK_PRINT is called.

--- trunk/libclock/libclock.h
+++ trunk/libclock/libclock.h
@@ -9,18 +9,29 @@
 	#include <windows.h>
 	#include <mmsystem.h>
 	#pragma comment (lib, "winmm.lib")      // link with my own library libfps
+	
+	#if	0	// MOD-BY-LEETEN 02/24/2011-FROM:
+		#define LIBCLOCK_INIT(flag, header)						\
+			{															\
+				const int _iMaxNrOfTimers = MAX_NR_OF_TIMERS;			\
+				DWORD _pdBeginTimers[_iMaxNrOfTimers];					\
+				DWORD _pdEndTimers[_iMaxNrOfTimers];					\
+				size_t _t = 0;											\
+				if( (flag)	)											\
+				{														\
+					printf("%s", header);								\
+				}
 
+	#else	// MOD-BY-LEETEN 02/24/2011-TO:
 	#define LIBCLOCK_INIT(flag, header)						\
 		{															\
 			const int _iMaxNrOfTimers = MAX_NR_OF_TIMERS;			\
 			DWORD _pdBeginTimers[_iMaxNrOfTimers];					\
 			DWORD _pdEndTimers[_iMaxNrOfTimers];					\
-			size_t _t = 0;											\
-			if( (flag)	)											\
-			{														\
-				printf("%s", header);								\
-			}
+			char* szHeader = header;								\
+			size_t _t = 0;											
 
+	#endif	// MOD-BY-LEETEN 02/24/2011-END
 
 	#define LIBCLOCK_BEGIN(flag)								\
 			if( (flag)	)											\
@@ -35,34 +46,63 @@
 				assert(_t < _iMaxNrOfTimers );						\
 			}
 
+	#if	0	// MOD-BY-LEETEN 02/24/2011-FROM:
+		#define LIBCLOCK_PRINT(flag)									\
+				if( (flag)	)										\
+				{													\
+					double dTotalTime = 0.0;							\
+					for(size_t i=0; i<_t; i++)						\
+					{												\
+						double dTime = (double)(_pdEndTimers[i] - _pdBeginTimers[i]);	\
+						printf("%.2f,", dTime);						\
+						dTotalTime += dTime;						\
+					}												\
+					printf("%.2f\n", dTotalTime);					\
+				}													\
+			}
+
+	#else	// MOD-BY-LEETEN 02/24/2011-TO:
 	#define LIBCLOCK_PRINT(flag)									\
-			if( (flag)	)										\
-			{													\
+			if( (flag)	)											\
+			{														\
+				printf("%s: ", szHeader);								\
 				double dTotalTime = 0.0;							\
-				for(size_t i=0; i<_t; i++)						\
-				{												\
+				for(size_t i=0; i<_t; i++)							\
+				{													\
 					double dTime = (double)(_pdEndTimers[i] - _pdBeginTimers[i]);	\
-					printf("%.2f,", dTime);						\
-					dTotalTime += dTime;						\
-				}												\
-				printf("%.2f\n", dTotalTime);					\
-			}													\
+					printf("%.2f,", dTime);							\
+					dTotalTime += dTime;							\
+				}													\
+				printf("%.2f\n", dTotalTime);						\
+			}														\
 		}
+
+	#endif	// MOD-BY-LEETEN 02/24/2011-END
 
 #else	// #ifdef WIN32
 	#include <time.h>
 	#pragma message("Remerber to add the argument '-l rt' to the linker options.")
 
+	#if	0	// MOD-BY-LEETEN 02/24/2011-FROM:
+		#define LIBCLOCK_INIT(flag, header)							\
+			{																\
+				static struct timespec _pcBeginTimers[MAX_NR_OF_TIMERS];	\
+				static struct timespec _pcEndTimers[MAX_NR_OF_TIMERS];		\
+				size_t _t = 0;												\
+				if( (flag)	)												\
+				{															\
+					printf("%s", header);									\
+				}
+
+	#else
 	#define LIBCLOCK_INIT(flag, header)							\
 		{																\
 			static struct timespec _pcBeginTimers[MAX_NR_OF_TIMERS];	\
 			static struct timespec _pcEndTimers[MAX_NR_OF_TIMERS];		\
 			size_t _t = 0;												\
-			if( (flag)	)												\
-			{															\
-				printf("%s", header);									\
-			}
+			char* szHeader = header;									
 
+	#endif
 
 	#define LIBCLOCK_BEGIN(flag)								\
 			if( (flag)	)											\
@@ -77,9 +117,27 @@
 				assert(_t < MAX_NR_OF_TIMERS);						\
 			}
 
+	#if	0	// MOD-BY-LEETEN 02/24/2011-FROM:
+		#define LIBCLOCK_PRINT(flag)							\
+				if( (flag)	)										\
+				{													\
+					double dTotalTime = 0.0;						\
+					for(size_t i=0; i<_t; i++)						\
+					{												\
+						double dBeginTime = (double)_pcBeginTimers[i].tv_sec * 1.0e+3 + (double)_pcBeginTimers[i].tv_nsec/1.0e+6;	\
+						double dEndTime = (double)_pcEndTimers[i].tv_sec * 1.0e+3 + (double)_pcEndTimers[i].tv_nsec/1.0e+6;			\
+						double dTime = dEndTime - dBeginTime;		\
+						printf("%.2f,", dTime);						\
+						dTotalTime  += dTime;						\
+					}												\
+					printf("%.2f\n", dTotalTime);					\
+				}													\
+			}
+	#else	// MOD-BY-LEETEN 02/24/2011-TO:
 	#define LIBCLOCK_PRINT(flag)							\
 			if( (flag)	)										\
 			{													\
+				printf("%s: ", szHeader);						\
 				double dTotalTime = 0.0;						\
 				for(size_t i=0; i<_t; i++)						\
 				{												\
@@ -92,6 +150,7 @@
 				printf("%.2f\n", dTotalTime);					\
 			}													\
 		}
+	#endif	// MOD-BY-LEETEN 02/24/2011-END
 
 #endif	// #ifdef WIN32
 
@@ -99,5 +158,10 @@
 /*
 
 $Log: not supported by cvs2svn $
+Revision 1.1  2011/01/10 19:35:45  leeten
+
+[01/09/2010]
+1. [1ST] First time checkin.
+
 
 */
