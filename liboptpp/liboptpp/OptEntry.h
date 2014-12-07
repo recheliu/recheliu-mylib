c68caaf7aae6b490eb2f4536c6c220dec14f133b
leeten <leeten@abdb6057-6f73-fb73-b203-3162f873fe11>
2014-12-07 10:54:34 +0000

    [2014/12/06] modified   example/liboptpp_example.cpp modified   liboptpp/OptBoolean.h modified   liboptpp/OptCharPtrVector.h modified   liboptpp/OptEntry.h modified   liboptpp/OptEnum.h modified   liboptpp/OptParser.h modified   liboptpp/OptPrimitiveTypeVector.h modified   liboptpp/OptStringVector.h modified   liboptpp/OptValueVector.h 1. [DEL] Remove name/date labels.

--- liboptpp/liboptpp/OptEntry.h
+++ liboptpp/liboptpp/OptEntry.h
@@ -17,9 +17,7 @@ using namespace std;
 #include <memory.h>
 #include <string.h>
 
-// ADD-BY-LEETEN 2014/12/06-BEGIN
 #include "liblog.h"
-// ADD-BY-LEETEN 2014/12/06-END
 
 namespace OptExt {
 	struct COptEntry 
@@ -27,26 +25,11 @@ namespace OptExt {
 		string szArgName;
 		string szDescription;
 
-		// ADD-BY-LEETEN 2014/12/06-BEGIN
 		void _PrintVarValue(const string& szTypeName, const string& szVarName, const string& szVarValue) const
 		{
 			cerr<<"\t"<<szTypeName<<" "<<szVarName<<" = "<<szVarValue<<";"<<endl;
 		}
-		// ADD-BY-LEETEN 2014/12/06-END
 
-		// ADD-BY-LEETEN 2014/12/06-BEGIN
-		#if	0	// MOD-BY-LEETEN 2014/12/06-FROM:
-		void setDescription(string szDescription) 
-		{
-			this->szDescription = szDescription;
-		}
-
-		COptEntry* withDescription(string szDescription) 
-		{
-			setDescription(szDescription);
-			return this;
-		}
-		#else	// MOD-BY-LEETEN 2014/12/06-TO:
 		void setDescription(const string& szDescription) 
 		{
 			this->szDescription = szDescription;
@@ -57,17 +40,9 @@ namespace OptExt {
 			setDescription(szDescription);
 			return this;
 		}
-		#endif	// MOD-BY-LEETEN 2014/12/06-END
-		// ADD-BY-LEETEN 2014/12/06-END
 
-		// ADD-BY-LEETEN 2014/12/06-BEGIN
 		//! Print the current value of the specified variable (*pDest) with the name szName.
-		// ADD-BY-LEETEN 2014/12/06-END
-		#if	0	// MOD-BY-LEETEN 2014/12/06-FROM:
-		virtual void _Print(string szName, void* pDest) const = 0;
-		#else	// MOD-BY-LEETEN 2014/12/06-TO:
 		virtual void _Print(const string& szName, void* pDest) const = 0;
-		#endif	// MOD-BY-LEETEN 2014/12/06-END
 
 		virtual void _PrintUsage()
 		{
@@ -77,22 +52,11 @@ namespace OptExt {
 		void _CheckArgumentNumbers(size_t uIndex, size_t uNrOfValues, size_t uNrOfArguments) 
 		{
 					// check the number of parameters
-			// MOD-BY-LEETEN 2014/12/06-FROM:
-			/*
-			if( uIndex + uNrOfValues >= uNrOfArguments) 
-			{
-				throw new invalid_argument("Too few parameters for " + this->szArgName);
-			}
-			*/
-			// MOD-BY-LEETEN 2014/12/06-TO:
 			ASSERT_OR_LOG(uIndex + uNrOfValues < uNrOfArguments, cerr<<"Too few parameters for " + this->szArgName);
-			// MOD-BY-LEETEN 2014/12/06-END
 		}
 
 
-		// ADD-BY-LEETEN 2014/12/06-BEGIN
 		//! Parse the value from *piStart.
-		// ADD-BY-LEETEN 2014/12/06-END
 		virtual void _Parse(size_t argc, char* argv[], int* piStart) = 0;
 
 		COptEntry():
@@ -101,11 +65,7 @@ namespace OptExt {
 		{
 		}
 
-		#if	0	// MOD-BY-LEETEN 2014/12/06-FROM:
-		COptEntry(string szArgName):
-		#else	// MOD-BY-LEETEN 2014/12/06-TO:
 		COptEntry(const string& szArgName):
-		#endif	// MOD-BY-LEETEN 2014/12/06-END
 			szArgName(szArgName),
 			szDescription("")
 		{
